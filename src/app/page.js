435379d6d60c73439c2f9bf7232bf77d1b206751
bobcoi03 <elongatedmusk6969@gmail.com>
2023-06-03 22:19:50 +0100

    responsive

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
       <div className="hero min-h-screen bg-base-200">
         <div className="hero-content text-center flex-col lg:flex-row-reverse">
           <div className="max-w-md">
-            <h1 className="text-4xl font-bold text-black">Daily photo challenges with friends.</h1>
+            <h1 className="text-4xl font-bold text-black text-center">Daily photo challenges with friends.</h1>
             <p className="py-6">Everyday, there's a new photo challenge that will run for 24 hours. Friends share photos according to the challenge that is designed to promote positivity, authenticity and good vibes😎</p>
             <span className='flex flex-row gap-3 items-center justify-center'>
               <a className="btn btn-secondary" href='https://apps.apple.com/us/app/peng6/id6446037266' target='_blank'>App store</a>
@@ -15,7 +15,7 @@ export default function Home() {
               </a>
             </span>
           </div>
-          <img src="/cropped-screenshot.png" className="max-w-sm h-1/2"/>
+          <img src="/cropped-screenshot.png" className="max-w-full sm:max-w-sm h-1/2"/>
         </div>
       </div>
     </main>
